72eb712895297383be43ce95f7d5f5926eb03c13
GARfIlD <v.a.bogush@gmail.com>
2018-12-24 22:53:00 +0300

    Initial commit

---
+++ app/src/main/java/by/bossoftware/soft/beatbox/BeatBoxFragment.java
@@ -0,0 +1,9 @@
+package by.bossoftware.soft.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
